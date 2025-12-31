d3af6b1d4cefebdce9b99343c87b57412e2f4e21
willow-12 <yosefayalew46@gmail.com>
2025-12-31 01:55:50 -0800

    Remove database password from DBConnection class

--- src/main/java/com/example/advice/DBConnection.java
+++ src/main/java/com/example/advice/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/confession_app";
     private static final String USER = "root";
-    private static final String PASSWORD = "yourpassword";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
